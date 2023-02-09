4be70ce3f67f3bf195c71a9b560b70f02e00d322
sunnymasih0253 <sunnyk.mdb@gmail.com>
2023-02-09 12:49:32 +0530

    update footer code

--- components/header-footer/footer.js
+++ components/header-footer/footer.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
-    <footer className='bg-black-v-4 py-14 dark'>
+    <footer className=' py-14 '>
         <div className="container">
-            <div class="flex ...">
-                <div class="max-w-[75%] w-full grid gap-12 grid-cols-4">
+            <div className="flex flex-wrap gap-3">
+                <div className="max-w-full lg:max-w-[72%] w-full grid gap-0 sm:gap-12 grid-cols-1 sm:grid-cols-2 md:grid-cols-4 ">
                     <div>
                         <h4 className='info-14-20 mb-6'>Company</h4>
-                        <ul>
+                        <ul className="h-0 overflow-hidden sm:h-auto">
                             <li className='mb-5'>
                                 <Link href="#" className='info-14 '>About Us</Link>
                             </li>
@@ -30,9 +31,10 @@ const Footer = () => {
                             </li>
                         </ul>
                     </div>
+                    
                     <div>
                         <h4 className='info-14-20 mb-6'>Products</h4>
-                        <ul>
+                        <ul className="h-0 overflow-hidden sm:h-auto">
                             <li className='mb-5'>
                                 <Link href="#" className='info-14 '>Buy Crypto</Link>
                             </li>
@@ -47,9 +49,10 @@ const Footer = () => {
                             </li>
                         </ul>
                     </div>
+                    
                     <div>
                         <h4 className='info-14-20 mb-6'>Service</h4>
-                        <ul>
+                        <ul className="h-0 overflow-hidden sm:h-auto">
                             <li className='mb-5'>
                                 <Link href="#" className='info-14 '>Bitget Academy</Link>
                             </li>
@@ -76,9 +79,10 @@ const Footer = () => {
                             </li>
                         </ul>
                     </div>
+                    
                     <div>
                         <h4 className='info-14-20 mb-6'>Legal & Disclosures</h4>
-                        <ul>
+                        <ul className="h-0 overflow-hidden sm:h-auto">
                             <li className='mb-5'>
                                 <Link href="#" className='info-14 '>Regulatory License</Link>
                             </li>
@@ -99,10 +103,10 @@ const Footer = () => {
                             </li>
                         </ul>
                     </div>
-
+                    
                     <div>
                         <h4 className='info-14-20 mb-6'>Support</h4>
-                        <ul>
+                        <ul className="h-0 overflow-hidden sm:h-auto">
                             <li className='mb-5'>
                                 <Link href="#" className='info-14 '>Feedback</Link>
                             </li>
@@ -121,12 +125,12 @@ const Footer = () => {
                            
                         </ul>
                     </div>
-
+                    
                     <div>
                         <h4 className='info-14-20 mb-6'>Copy Trading</h4>
-                        <ul>
+                        <ul className="h-0 overflow-hidden sm:h-auto">
                             <li className='mb-5'>
-                                <Link href="#" className='info-14 '>Copy Trading</Link>
+                                <Link href="#" className='info-14'>Copy Trading</Link>
                             </li>
                             <li className='mb-5'>
                                 <Link href="#" className='info-14 '>Strategy Trading</Link>
@@ -134,12 +138,29 @@ const Footer = () => {
                            
                         </ul>
                     </div>
-                
-                  
-
                 </div>
-                <div class="max-w-[25%] w-full">
-
+                <div className=" max-w-full lg:max-w-[22%] w-full">
+                    <div>
+                        {/* <Image src="" alt='error' width="132" height="53" /> */}
+                    </div>
+                    <div className='my-5'>
+                        <div className="mb-4">
+                            <p className='info-14-16'>Support Service</p>
+                            <Link href="mailto:support@bitget.com" className='info-14'>support@bitget.com</Link>
+                        </div>
+                        <div className="mb-4">
+                            <p className='info-14-16'>Business Opportunity</p>
+                            <Link href="mailto:BD@bitget.com" className='info-14'>BD@bitget.com</Link>
+                        </div>
+                        <div className="mb-4">
+                            <p className='info-14-16'>Trading Quant & MM</p>
+                            <Link href="mailto:institution@bitget.com" className='info-14'>institution@bitget.com</Link>
+                        </div>
+                        <div className="mb-4">
+                            <p className='info-14-16'>VIP Services</p>
+                            <Link href="mailto:vip@bitget.com" className='info-14'>vip@bitget.com</Link>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
