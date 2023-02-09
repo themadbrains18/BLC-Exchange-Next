1d365672c3b0204e207356655b2e74a44c169ccb
Princepapneja <princepapneja.mdb@gmail.com>
2023-02-09 12:12:16 +0530

    header with logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '@/styles/globals.css'
 import Footer from '@/components/header-footer/footer'
+import Header from '@/components/header-footer/header'
 
 export default function App({ Component, pageProps }) {
   return(
     <>
+    <Header/>
       <Component {...pageProps} />
       <Footer />
     </>
