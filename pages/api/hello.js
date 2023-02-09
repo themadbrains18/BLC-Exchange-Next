abfd4583b64cc5668209e5e98378c3364dea70c2
sunnymasih0253 <sunnyk.mdb@gmail.com>
2023-02-09 18:52:38 +0530

    merge code

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,8 +4,8 @@ import { promises as fs }  from 'fs'
 
 export default async function handler(req, res) {
   
-  const nav = await fs.readFile(process.cwd()+'/staticData/header-footer/nav.json',"utf8");
-  const footerNav = await fs.readFile(process.cwd()+'/staticData/header-footer/footer.json',"utf8",{});
+  const nav = await fs.readFile(process.cwd()+'/staticData/nav.json',"utf8");
+  const footerNav = await fs.readFile(process.cwd()+'/staticData/footer.json',"utf8",{});
 
   res.status(200).json({
     nav : JSON.parse(nav),
