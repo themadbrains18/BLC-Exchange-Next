e89e1af25eccaf19cc2a1ec1d418549663de004e
SakshiSethi <sakshisethi.mdb@gmail.com>
2023-02-13 15:18:21 +0530

    create snippet of swiper

---
+++ components/home/tutorials.js
@@ -0,0 +1,39 @@
+
+import Carousel from '../snippets/carousel';
+import { useEffect, useState } from 'react';
+
+const Tutorials = () => {
+
+
+    const [Data, setData] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            await fetch("http://localhost:3000/api/hello")
+                .then((res) => res.json())
+                .then((data) => {
+
+                    setData(data.tutorials);
+
+                    /* process your data further */
+                })
+                .catch((error) => console.error(error));
+        })().catch((err) => {
+            console.log(err);
+        });
+    }, []);
+    return (
+        <section className="ai_zone bg-black-v-5 py-[80px] md:py-[120px]">
+            <div className="container">
+                <div >
+                    <div className='hero-heading'>Your passport to the world of Crypto</div>
+                    <p className='info-14-20 mt-6 md:mt-6 mb-20'>Build your crypto knowledge by exploring the latest Bitget tutorials.</p>
+                </div>
+                <Carousel data={Data} play_cta={true}></Carousel>
+             
+            </div>
+        </section>
+    )
+}
+
+export default Tutorials;
