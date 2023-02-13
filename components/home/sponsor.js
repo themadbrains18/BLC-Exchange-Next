6735d022ae1560f225102084f587e2caf6606ce7
SakshiSethi <sakshisethi.mdb@gmail.com>
2023-02-13 17:01:40 +0530

    create sponsor component

--- components/home/sponsor.js
+++ components/home/sponsor.js
@@ -1,8 +1,4 @@
 import React from 'react'
-import Image from 'next/image';
-import Sponsor1 from '../../public/assets/sponsor1.png'
-import Sponsor2 from '../../public/assets/sponsor2.png'
-import Sponsor3 from '../../public/assets/sponsor3.png'
 
 let images = ["sponsor1", "sponsor2", "sponsor3"];
 
@@ -12,7 +8,6 @@ const Sponsor = () => {
             <div className="container flex justify-between p-9 items-center md:justify-around">
                 {images.map((e, i) => {
                     return (
-                        // <Image src={images[i]} alt="error"  className='rounded-xl my:0 lg:mx-40 items-center justify-center py-12 ' />
                         <img src={`/assets/${e}.png`} alt="" className='h-16 md:h-20 max-w-[80px] w-full  object-contain '/>
                     )
                 })
