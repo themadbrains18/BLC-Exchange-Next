d9d9491f6fabc63c0589d3ab2226e55a7c834698
SakshiSethi <sakshisethi.mdb@gmail.com>
2023-02-14 17:54:30 +0530

    complete about us page

--- components/about/aboutus.js
+++ components/about/aboutus.js
@@ -3,19 +3,19 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    <section className='bg-[#f8f8f8] dark:bg-black-v-5 py-[80px] md:py-[120px]  '>
+    <section className='bg-[#f8f8f8] dark:bg-black-v-5 py-[50px] md:py-[120px]  '>
     <div className="container ">
-        <h1 className='dark:text-white text-4xl leading-10 font-bold mb-6 lg:mb-0'>About Us</h1>
+        <h1 className='dark:text-white text-2xl font-semibold md:text-4xl leading-10 md:font-bold mb-6 lg:mb-0'>About Us</h1>
         <div className='flex justify-around items-center  '>
             <Image src='/assets/images/about_us.png' width={332} height={380} alt='error' className='hidden lg:block'></Image>
             <div className='max-w-full lg:max-w-xl w-full'>
-            <p className='info-14-20'>
+            <p className='text-base md:info-14-20'>
             Launched in 2018, Bitget provides comprehensive and secure trading solutions to our global userbase.
             </p>
-            <p className='my-8 info-14-20'>
+            <p className='my-8 text-base md:info-14-20'>
             With a core focus on social trading, Bitget has amassed over 8 million users, 1.1 million followers, and 55,000 professional traders from more than 100 countries around the world.
             </p>
-            <p className='info-14-20'>
+            <p className='text-base md:info-14-20'>
             Bitget is ranked in the top five globally by CoinGecko for derivatives trading by volume, and has generated over US$100 billion in trading volume in 2021 alone.
             </p>
 
@@ -24,20 +24,20 @@ const AboutUs = () => {
         </div>
         <div className='grid grid-cols-1  md:grid md:grid-cols-2 bg-[#f8f8f8] lg:flex justify-between mt-16 rounded-2xl lg:bg-white lg:py-16 lg:px-20'>
            <div className='mb-10 md:mb-16 lg:flex flex-col justify-between'>
-            <p className='section-secondary-heading font-noto text-primary font-bold'>#1 Platform</p>
-            <p className='mt-4 info-14-16'>For copy trading by volume</p>
+            <p className='text-2xl md:section-secondary-heading font-noto text-primary dark:text-primary font-bold'>#1 Platform</p>
+            <p className='mt-4 text-base md:info-14-16 dark:text-black'>For copy trading by volume</p>
            </div>
            <div className='mb-10 md:mb-16 lg:flex flex-col justify-between'>
-            <p className='section-secondary-heading font-noto text-primary font-bold'>Top 5</p>
-            <p className='mt-4 info-14-16'>In derivatives by trading volume</p>
+            <p className='text-2xl md:section-secondary-heading font-noto text-primary dark:text-primary font-bold'>Top 5</p>
+            <p className='mt-4 info-14-16 dark:text-black'>In derivatives by trading volume</p>
            </div>
            <div className='mb-10 md:mb-16 lg:flex flex-col justify-between'>
-            <p className='section-secondary-heading font-noto text-primary font-bold'>8+ million</p>
-            <p className='mt-4 info-14-16'>Registered users</p>
+            <p className='text-2xl md:section-secondary-heading font-noto text-primary  dark:text-primary font-bold'>8+ million</p>
+            <p className='mt-4 text-base md:info-14-16 dark:text-black'>Registered users</p>
            </div>
            <div className='mb-10 md:mb-16 lg:flex flex-col justify-between'>
-            <p className='section-secondary-heading font-noto text-primary font-bold'>US $10 Billion</p>
-            <p className='mt-4 info-14-16'>Average daily trading volume</p>
+            <p className='section-secondary-heading font-noto text-primary  dark:text-primary font-bold'>US $10 Billion</p>
+            <p className='mt-4 text-base md:info-14-16 dark:text-black'>Average daily trading volume</p>
            </div>
         </div>
     </div>
--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,12 @@
 import AboutUs from "@/components/about/aboutus"
 import Future from "@/components/about/future"
+import Journey from "@/components/about/journey"
+import Links from "@/components/about/links"
 import Mission from "@/components/about/mission"
 import Partners from "@/components/about/partners"
+import Team from "@/components/about/team"
 import Values from "@/components/about/values"
+import WhyChoose from "@/components/about/whyChoose"
 
 export default function About() {
   return (
@@ -13,6 +17,10 @@ export default function About() {
       <AboutUs />
       <Future />
       <Partners />
+      <Team />
+      <WhyChoose />
+      <Links />
+      <Journey />
       </div>
     </>
   )
