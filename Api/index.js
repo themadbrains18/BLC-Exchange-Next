817464bbf19a079fb21b9eecd648babd4a4293f2
surindermdb <surinderkumar.mdb@gmail.com>
2023-02-18 17:47:06 +0530

    change base url

--- Api/index.js
+++ Api/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const baseurl = 'http://localhost:5000/api';
+// const baseurl = 'http://http://localhost:5000/api-blc';
+const baseurl = 'http://http://demo.blcexchange.net/api-blc';
 
 const API = axios.create(
   {
@@ -10,8 +11,8 @@ const API = axios.create(
 
 export const loginRequestApi = (formData) => API.post('/users/login', formData)
 export const registerRequest = (formData) => API.post('/users/create', formData)
-export const checkUserRequest = (formData) => API.post('/users',formData);
+export const checkUserRequest = (formData) => API.post('/users', formData);
 
 export const sendOtp = (formData) => API.post('/otp', formData)
 
-export const otpMatchRequestApi =(formData) => API.post('/otp/match', formData);
+export const otpMatchRequestApi = (formData) => API.post('/otp/match', formData);
