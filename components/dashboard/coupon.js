6ddb2ab7b815f04a3125060f90c9061f8c1540c1
SakshiSethi <sakshisethi.mdb@gmail.com>
2023-02-22 13:24:24 +0530

    complete coupon or competition tab

--- components/dashboard/coupon.js
+++ components/dashboard/coupon.js
@@ -26,18 +26,18 @@ const Coupon = () => {
     <section className="w-full px-[24px]">
       <div>
         <p className='section-secondary-heading'>Coupons Center</p>
-        <div className='flex justify-between'>
+        <div className='flex flex-col lg:flex-row justify-between'>
           <div className='flex gap-36 '>
-            <div className='mt-8 cursor-pointer'>
+            <div className='mt-8 cursor-pointer' onClick={()=>{setActive(0)}}>
               <p className='info-14-16'>Available Coupons</p>
               <p className='section-secondary-heading text-primary'>0 <span className='info-14'>coupon(s)</span></p>
             </div>
-            <div className='mt-8 cursor-pointer'>
+            <div className='mt-8 cursor-pointer' onClick={()=>{setActive(1)}}>
               <p className='info-14-16'>Task Cards</p>
               <p className='section-secondary-heading text-primary'>0 <span className='info-14'>coupon(s)</span></p>
             </div>
           </div>
-          <div className='cursor-pointer'>
+          <div className='cursor-pointer hidden sm:block mt-5  lg:mt-0'>
             <img src='/assets/images/coupon.png' alt='coupon' className='max-h-32 h-full'></img>
           </div>
         </div>
@@ -51,7 +51,7 @@ const Coupon = () => {
             <div className='flex flex-col gap-4 mt-6 relative' onClick={(e) => { setShowId(!showId), setRotate(!rotate) }}>
               <label className='info-12'>Coupon type</label>
               <div className='flex border-2 border-border-clr  bg-transparent rounded-lg focus:!border-primary dark:border-white'>
-                <input type='text' placeholder='ID Card' autocomplete="off" value={coupon} className=' max-w-fit w-full p-2 -z-10  bg-transparent   text-black dark:text-white outline-none ' />
+                <input type='text' placeholder='ID Card' autocomplete="off" value={coupon} className=' max-w-fit w-full p-2 info-14 bg-transparent   text-black dark:text-white outline-none ' />
                 <img
                   src="/assets/icons/down.svg"
                   className={`${rotate && "rotate-90"} duration-300`}
@@ -61,8 +61,8 @@ const Coupon = () => {
             </div>
             <div className='flex flex-col gap-4 mt-6 relative' onClick={(e) => { setShowDropdown(!showDropdown), setRotate1(!rotate1) }}>
               <label className='info-12'>Coupon status</label>
-              <div className='flex border-2 border-border-clr  bg-transparent rounded-lg focus:!border-primary dark:border-white'>
-                <input type='text' placeholder='ID Card' autocomplete="off" value={couponStatus} className=' max-w-fit w-full p-2 -z-10  bg-transparent   text-black dark:text-white outline-none ' />
+              <div className='flex border-2 border-border-clr  rounded-lg focus:!border-primary dark:border-white'>
+                <input type='text' placeholder='ID Card' autocomplete="off" value={couponStatus} className=' max-w-fit w-full p-2  info-14  bg-transparent   text-black dark:text-white outline-none ' />
                 <img
                   src="/assets/icons/down.svg"
                   className={`${rotate1 && "rotate-90"} duration-300`}
