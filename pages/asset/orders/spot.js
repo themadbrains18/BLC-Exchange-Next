0632d0e3fb4bebb25af81128beda8208c699f300
Princepapneja <princepapneja.mdb@gmail.com>
2023-02-23 16:11:02 +0530

    spopt orders complete

--- pages/asset/orders/spot.js
+++ pages/asset/orders/spot.js
@@ -1,5 +1,5 @@
 import Layout from "/components/layout/layout";
-import { useState,useContext } from "react";
+import { useState, useContext } from "react";
 import { getProviders, getSession } from "next-auth/react";
 import ActiveCta from "/components/snippets/activeCta";
 import SelectMenu from "@/components/snippets/selectMenu";
@@ -11,10 +11,11 @@ const Spot = ({ assets }) => {
   let activeCta = ["Open Orders", "Order History", "Trading Details"];
   let selectMenu = ["Limit Orders", "Trigger Orders", "OCO"];
   let coinList = ["All", "BTC", "OCO"];
+  let dateFilter = ["Last 7 Days", "Last 30 Days"];
   const [active, setActive] = useState(0);
   const [data, setData] = useState(false);
   const [fliterShow, setFliterShow] = useState(false);
-  const { setClick,mode } = useContext(Context);
+  const { setClick, mode } = useContext(Context);
   return (
     <>
       <Layout data={assets} name="Spot">
@@ -23,8 +24,15 @@ const Spot = ({ assets }) => {
           <div className="mt-4">
             <ActiveCta data={activeCta} active={active} setActive={setActive} />
           </div>
-          <div className="mt-2 md:mt-4 md:bg-table-bg p-2 flex justify-between lg:justify-start gap-4">
+          <div className="flex justify-between items-center gap-3 md:bg-table-bg mt-2 md:mt-4 ">
+
+         
+          <div className="  p-2 flex justify-between lg:justify-start gap-4 md:flex-wrap grow">
+           {
+            active!=2 &&
             <SelectMenu selectMenu={selectMenu} />
+           }
+          
             {/* search */}
             <div className="hidden lg:flex items-center gap-2">
               <div className="relative text-gray-600 focus-within:text-gray-400">
@@ -58,6 +66,9 @@ const Spot = ({ assets }) => {
               <SelectMenu selectMenu={coinList} />
               <button className="cta leading-8 h-full">Search</button>
             </div>
+            {
+              active!=0 && <SelectMenu selectMenu={dateFilter} />
+            }
             <button
               className="lg:hidden"
               onClick={() => {
@@ -71,43 +82,64 @@ const Spot = ({ assets }) => {
                 alt=""
               />
             </button>
-            {fliterShow && (
-              <div className="fixed bottom-0 right-0 w-full
-              ">
-                <div className="flex justify-between bg-white p-4 border-b md:hidden border-t border-border-clr">
-                <h3 className="info-14-20">Filter</h3>
-                <div>
-                <SelectMenu selectMenu={selectMenu} />
-                  
-                </div>
-            <button
-              className="ml-auto mr-0"
-              onClick={() => {
-                setShow(false);
-                setClick(false);
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke={mode==="dark"?"white":"currentcolor"}
-                className="w-6 h-6 mr-0 ml-auto "
+            
+              <div
+                className={`fixed -bottom-[100%] duration-500 right-0 w-full bg-white h-[100vh] ${fliterShow && "bottom-[0%] z-[4] "} `}
+              
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                ></path>
-              </svg>
-            </button>
-          </div>
+                <div className="flex justify-between mb-4  p-4 border-b md:hidden border-t border-border-clr">
+                  <h3 className="info-14-20">Filter</h3>
+
+                  <button
+                    className="ml-auto mr-0"
+                    onClick={() => {
+                      setFliterShow(false);
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke={mode === "dark" ? "white" : "currentcolor"}
+                      className="w-6 h-6 mr-0 ml-auto "
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+                <div className="mt-4 px-2">
+                  <SelectMenu selectMenu={selectMenu} />
+                </div>
+                <div className="mt-4 px-2">
+                  <SelectMenu selectMenu={coinList} />
+                </div>
+                <div className="mt-4 px-2 flex gap-4">
+                  <button className="cta2 w-full">Reset</button>
+                  <button className="cta w-full" >Confirm</button>
+                </div>
               </div>
-            )}
+    
           </div>
+          {
+            active!=0 &&
+            <button className="cta2 leading-8 bg-transparent whitespace-nowrap rounded-sm hidden lg:inline">Export Data</button>
+          }
+          
+          </div>
+          {
+            active!=0 &&
+            <div className="text-end lg:hidden">
+
+              <button className="transparent-cta  rounded-sm  ">Export Data</button>
+            </div>
+          }
           {/* table */}
-         <Table data={data}/>
+          <Table data={data} />
         </div>
       </Layout>
     </>
