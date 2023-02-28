61c11ecfa1fd1b82d87a8c746a5c4c46b8f946c9
Princepapneja <princepapneja.mdb@gmail.com>
2023-02-28 19:08:32 +0530

    till slider complete

---
+++ components/referal/stepsCard.js
@@ -0,0 +1,32 @@
+import React from "react";
+import StepsCardSnippet from "./stepsCardSnippet";
+
+const StepsCardSec = ({ cardData }) => {
+  return (
+    <>
+      <section className="py-10 md:py-20 dark:bg-black-v-5">
+        <div className="container">
+          <h4 className="section-secondary-heading font-noto text-center mb-4 md:mb-8">
+            How It Works?
+          </h4>
+          <div className="flex table_box gap-2 md:gap-4 justify-between px-4 py-4 ">
+            {cardData &&
+              cardData.map((e, i) => {
+                return (
+                  <StepsCardSnippet
+                    key={i}
+                    svgType={e.svgType}
+                    heading={e.heading}
+                    desc={e.desc}
+                    index={i + 1}
+                  />
+                );
+              })}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default StepsCardSec;
