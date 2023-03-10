a344ac0bd9922119a4772168568457502bdf3adf
sunnymasih0253 <sunnyk.mdb@gmail.com>
2023-03-10 20:06:26 +0530

    add chat box

---
+++ pages/p2p-trade/order.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import OrderSec from "components/oneClickBuy/order-sec";
+const Order = () => {
+  return (
+    <>
+        <OrderSec />
+    </>
+  )
+}
+
+export default Order;
