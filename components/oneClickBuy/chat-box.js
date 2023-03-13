59a7ef28d345ce767a28906d76cc3de0e8706be2
sunnymasih0253 <sunnyk.mdb@gmail.com>
2023-03-13 13:35:00 +0530

    add file upload in chat box

--- components/oneClickBuy/chat-box.js
+++ components/oneClickBuy/chat-box.js
@@ -36,6 +36,23 @@ const ChatBox = () => {
             <div className="border-t border-[#cccccc7d] p-[14px] !py-[7px]">
                 <div className="flex items-center gap-[15px]">
                     <input type="text" className="border-0 w-full outline-none info-12 dark:!text-white bg-transparent !text-black"  placeholder="input messsage..." />
+                    <input type="file" className="hidden" id="fileUpload" />
+                    <label htmlFor="fileUpload" className="cursor-pointer" > 
+                        <svg width="24px" height="20px" viewBox="0 0 24 20" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+                            <g id="Icons" stroke="none" strokeWidth={1} fill="none" fillRule="evenodd">
+                            <g id="Outlined" transform="translate(-610.000000, -376.000000)">
+                                <g id="Action" transform="translate(100.000000, 100.000000)">
+                                <g id="Outlined-/-Action-/-perm_media" transform="translate(510.000000, 274.000000)">
+                                    <g>
+                                    <polygon id="Path" points="0 0 24 0 24 24 0 24" />
+                                    <path d="M2,6 L0,6 L0,11 L0.01,11 L0,20 C0,21.1 0.9,22 2,22 L20,22 L20,20 L2,20 L2,6 Z M7,15 L21,15 L17.5,10.5 L15,13.51 L11.5,9 L7,15 Z M22,4 L14,4 L12,2 L6,2 C4.9,2 4.01,2.9 4.01,4 L4,16 C4,17.1 4.9,18 6,18 L22,18 C23.1,18 24,17.1 24,16 L24,6 C24,4.9 23.1,4 22,4 Z M22,16 L6,16 L6,4 L11.17,4 L12.58,5.41 L13.17,6 L22,6 L22,16 Z" id="🔹-Icon-Color" fill="#1D1D1D" />
+                                    </g>
+                                </g>
+                                </g>
+                            </g>
+                            </g>
+                        </svg>
+                    </label>
                     <button className="cta">Send</button>
                 </div>
             </div>
