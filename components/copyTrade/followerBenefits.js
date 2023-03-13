584b53bbb45cc0c1ec971946ce6406959ec67598
SunilTmb <sunilpb22@gmail.com>
2023-03-13 10:34:09 +0530

    copyTrade done

---
+++ components/copyTrade/followerBenefits.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function FollowerBenefits() {
+  return (
+    <section className='startTraderPrivilege dark:bg-black md:pt-[80px] pt-[40px]'>
+        <div className='container'>
+            <div className='text-center'>
+                <h2 className='heading-28-40 font-semibold capitalize '>Follower Benefits</h2>
+                <p className='info-14-16'>Copy traders and start earning with a peace of mind</p>
+            </div>
+            <div className='flex gap-4'>
+                <div className='max-w[]'>
+
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default FollowerBenefits
--- pages/copyTrade.js
+++ pages/copyTrade.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Banner from '@/components/copyTrade/banner'
 import TraderPrivilege from '@/components/copyTrade/traderPrivilege'
+import FollowerBenefits from '@/components/copyTrade/followerBenefits'
 
 function CopyTrade() {
   return (
-    < Banner/>,
-    < TraderPrivilege/>
+    <>
+      < Banner/>
+      < TraderPrivilege/>
+      < FollowerBenefits/>
+    </>
+    
+
   )
 }
 
