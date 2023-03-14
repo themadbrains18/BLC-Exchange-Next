b999d9a8a6166af12837b7e6c0016a2130e8728f
amit-mehta28 <amitmehta7973@gmail.com>
2023-03-14 12:00:13 +0530

    strategyTrading Page done (without dynamic)

--- components/strategyTrading/futureSpot.js
+++ components/strategyTrading/futureSpot.js
@@ -13,7 +13,7 @@ const FutureSpot = () => {
                     <div className='grid grid-cols-1 sm:grid-cols-2 gap-5 lg:grid-cols-4'>
                         <div className=' p-6 border-[1px] rounded-xl'>
                             <Image src={futureSpot1} width={72} height={72} className='mb-6' ></Image>
-                            <h3 className='font-noto text-2xl mb-6'>Futures grid</h3>
+                            <h3 className='font-noto  dark:text-white text-2xl mb-6'>Futures grid</h3>
                             <ul className='list-disc pl-4 mb-10'>
                                 <li className='info-14 font-medium'>Leverage your capital</li>
                                 <li className='info-14 font-medium'>Automate your trades even when prices fluctuate</li>
@@ -21,12 +21,12 @@ const FutureSpot = () => {
                             </ul>
                             <Link href={"#"} className="text-xl font-medium text-black  dark:text-primary flex items-center gap-2">
                                 Create
-                                <Image src={futureSpotArrow} width={26} height={15} className='h-[15px]' ></Image>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="16" fill="none"><path d="M24.746 8.707a1 1 0 000-1.414L18.383.929a1 1 0 00-1.415 1.414L22.625 8l-5.657 5.657a1 1 0 001.415 1.414l6.363-6.364zM.04 9h24V7h-24v2z" fill="#0D0E0E " className='dark:fill-white' /></svg>
                             </Link>
                         </div>
                         <div className=' p-6 border-[1px] rounded-xl'>
                             <Image src={futureSpot1} width={72} height={72} className='mb-6' ></Image>
-                            <h3 className='font-noto text-2xl mb-6'>Futures grid</h3>
+                            <h3 className='font-noto dark:text-white text-2xl mb-6'>Futures grid</h3>
                             <ul className='list-disc pl-4 mb-10'>
                                 <li className='info-14 font-medium'>Leverage your capital</li>
                                 <li className='info-14 font-medium'>Automate your trades even when prices fluctuate</li>
@@ -34,12 +34,12 @@ const FutureSpot = () => {
                             </ul>
                             <Link href={"#"} className="text-xl font-medium text-black  dark:text-primary flex items-center gap-2">
                                 Create
-                                <Image src={futureSpotArrow} width={26} height={15} className='h-[15px]' ></Image>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="16" fill="none"><path d="M24.746 8.707a1 1 0 000-1.414L18.383.929a1 1 0 00-1.415 1.414L22.625 8l-5.657 5.657a1 1 0 001.415 1.414l6.363-6.364zM.04 9h24V7h-24v2z" fill="#0D0E0E " className='dark:fill-white' /></svg>
                             </Link>
                         </div>
                         <div className=' p-6 border-[1px] rounded-xl'>
                             <Image src={futureSpot1} width={72} height={72} className='mb-6' ></Image>
-                            <h3 className='font-noto text-2xl mb-6'>Futures grid</h3>
+                            <h3 className='font-noto dark:text-white text-2xl mb-6'>Futures grid</h3>
                             <ul className='list-disc pl-4 mb-10'>
                                 <li className='info-14 font-medium'>Leverage your capital</li>
                                 <li className='info-14 font-medium'>Automate your trades even when prices fluctuate</li>
@@ -47,12 +47,12 @@ const FutureSpot = () => {
                             </ul>
                             <Link href={"#"} className="text-xl font-medium text-black  dark:text-primary flex items-center gap-2">
                                 Create
-                                <Image src={futureSpotArrow} width={26} height={15} className='h-[15px]' ></Image>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="16" fill="none"><path d="M24.746 8.707a1 1 0 000-1.414L18.383.929a1 1 0 00-1.415 1.414L22.625 8l-5.657 5.657a1 1 0 001.415 1.414l6.363-6.364zM.04 9h24V7h-24v2z" fill="#0D0E0E " className='dark:fill-white' /></svg>
                             </Link>
                         </div>
                         <div className=' p-6 border-[1px] rounded-xl'>
                             <Image src={futureSpot1} width={72} height={72} className='mb-6' ></Image>
-                            <h3 className='font-noto text-2xl mb-6'>Futures grid</h3>
+                            <h3 className='font-noto dark:text-white text-2xl mb-6'>Futures grid</h3>
                             <ul className='list-disc pl-4 mb-10'>
                                 <li className='info-14 font-medium'>Leverage your capital</li>
                                 <li className='info-14 font-medium'>Automate your trades even when prices fluctuate</li>
@@ -60,7 +60,8 @@ const FutureSpot = () => {
                             </ul>
                             <Link href={"#"} className="text-xl font-medium text-black  dark:text-primary flex items-center gap-2">
                                 Create
-                                <Image src={futureSpotArrow} width={26} height={15} className='h-[15px]' ></Image>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="26" height="16" fill="none"><path d="M24.746 8.707a1 1 0 000-1.414L18.383.929a1 1 0 00-1.415 1.414L22.625 8l-5.657 5.657a1 1 0 001.415 1.414l6.363-6.364zM.04 9h24V7h-24v2z" fill="#0D0E0E " className='dark:fill-white' /></svg>
+                                {/* <Image src={futureSpotArrow} width={26} height={15} className='h-[15px] dark:fill-white'></Image> */}
                             </Link>
                         </div>
 
